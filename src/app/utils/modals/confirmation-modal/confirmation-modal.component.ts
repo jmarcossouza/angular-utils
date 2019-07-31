1f4732fe3c208a910cb7b8660ce12b581ca9935b
João Marcos A. Souza <contato@jmarcossouza.com>
2019-07-31 15:58:05 -0300

    Emissão de eventos do modal funcionando como deveria (teste 1)

--- src/app/utils/modals/confirmation-modal/confirmation-modal.component.ts
+++ src/app/utils/modals/confirmation-modal/confirmation-modal.component.ts
@@ -1,43 +1,29 @@
-import { Component, ViewChild, AfterViewInit, TemplateRef, ChangeDetectorRef, Output, EventEmitter } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, TemplateRef, ChangeDetectorRef, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { BsModalService, BsModalRef, ModalDirective } from 'ngx-bootstrap/modal';
 import { ModalsUtils } from '../ModalsUtils';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
     selector: 'uts-confirmation-modal',
     templateUrl: './confirmation-modal.component.html',
     styles: []
 })
-export class ConfirmationModalComponent implements AfterViewInit {
+export class ConfirmationModalComponent implements AfterViewInit, OnDestroy {
 
-    @ViewChild(ModalDirective, { static: false }) private modalDirective: ModalDirective;
+    @ViewChild(ModalDirective, { static: false }) public modalDirective: ModalDirective;
     @Output() confirmation: EventEmitter<boolean> = new EventEmitter<boolean>();
-    private modalRef: BsModalRef;
+    private onHideSubscription: Subscription;
 
-    constructor(private bsModalService: BsModalService, private changeDetection: ChangeDetectorRef) {
+    constructor() {
         ModalsUtils.confirmationModalComponent = this;
     }
 
     ngAfterViewInit(): void {
-        console.log(this.modalDirective);
+        this.onHideSubscription = this.modalDirective.onHide.subscribe(() => this.confirmation.emit(false));
     }
 
-    private _confirm(): void {
-        this.modalDirective.hide();
-        this.confirmation.emit(true);
-    }
-
-    public confirm(): void {
-        this._confirm();
-    }
-
-    private _decline(): void {
-        this.modalDirective.hide();
-        this.confirmation.emit(false);
-    }
-
-    public decline(): void {
-        this._decline();
+    ngOnDestroy() {
+        this.onHideSubscription.unsubscribe();
     }
 
     public openModal(): Observable<boolean> {
@@ -45,7 +31,4 @@ export class ConfirmationModalComponent implements AfterViewInit {
         this.confirmation = new EventEmitter<boolean>(); //Não sei se isso é necessário, mas é pra toda vez "resetar" a instancia do EventEmitter.
         return this.confirmation;
     }
-
-
-
 }
